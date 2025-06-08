57982d3a9f854e7f61d1496d995bb046d9cf869e
Marcin Dulak <marcindulak@users.noreply.github.com>
2025-06-08 16:27:01 +0200

    Run gha at 9 a.m. UTC

--- 02.4-create-a-new-partition.sh
+++ 02.4-create-a-new-partition.sh
@@ -18,10 +18,13 @@ IMAGE_SIZE=$1
 for dev in $(losetup -n -l -O NAME -j /vagrant/build_dir.img); do echo "Detaching $dev" && losetup -d $dev; done
 for dev in $(losetup -n -l -O NAME -j /build_dir.img); do echo "Detaching $dev" && sudo losetup -d $dev; done
 rm -f /vagrant/build_dir.img
-dd if=/dev/zero of=/vagrant/build_dir.img bs=1024M count=$IMAGE_SIZE
-# /vagrant synced-folder cannot be used for loop devices, but docker allows for this
+dd if=/dev/zero of=/vagrant/build_dir.img bs=1024M count="$IMAGE_SIZE"
+# /vagrant synced-folder cannot be used for loop devices, but docker allowed for this until sime time like 2025-06 (Ubuntu 24.04)
+# The current fix inside of docker is to use mknod to explicitly create the next available device
 # losetup: /vagrant/build_dir.img: failed to set up loop device: No such file or directory
-losetup -fP /vagrant/build_dir.img
+LOOPDEV=$(losetup -f)
+[ -e "$LOOPDEV" ] || mknod -m660 "$LOOPDEV" b 7 "${LOOPDEV#/dev/loop}"
+losetup "$LOOPDEV" /vagrant/build_dir.img
 losetup -n -l -O NAME -j /vagrant/build_dir.img > build_dir.dev
 parted -s $(cat build_dir.dev) mklabel msdos
 parted -s $(cat build_dir.dev) print free
